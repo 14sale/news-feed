1c9b46a276a22c752be51b88e93c968743f56516
Sasa Samardzic <sasa.samardzic@bravosystems.com>
2020-10-26 13:47:59 +0100

    Refactoring

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,29 +5,38 @@ import { dateFormat } from '../../utils/HelperFunctions'
 
 const Article = props => {
   const {
-    urlToImage,
-    title,
-    publishedAt,
-    published = dateFormat(publishedAt),
-    author,
-    content
-  } = props.article;
+    article: { urlToImage },
+    article: { title },
+    article: { publishedAt },
+    article: { published = dateFormat(publishedAt) },
+    article: { author },
+    article: { content },
+    backFromPreview,
+    backFromPreviewText,
+  } = props
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0)
   })
 
   return (
     <ContentWrapepr>
-      <button className={styles.backToList} onClick={() => props.backFromPreview()}><i className="arrow left" />{props.backFromPreviewText ?? 'Back'}</button>
+      <button type="button" className={styles.backToList} onClick={backFromPreview}>
+        <i className="arrow left" />
+        {backFromPreviewText || 'Back'}
+      </button>
+
       <h1 className="page-title">{title}</h1>
+
       <div className={styles.meta}>
         <p>Published: {published} by {author}</p>
       </div>
+
       <div className={styles.content}>
         <img src={urlToImage} alt={title} />
         <p>{content}</p>
       </div>
+
     </ContentWrapepr>
   )
 }
